596988aea652391be6dd2d42ef12ff35048b94b2
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2025-01-17 15:07:24 +0530

    feat(navbar): adds placeholder submenu for quicklinks

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,17 +5,26 @@ import RocketLogo from '../../assets/images/rocket.svg'
 import { MdSegment } from 'react-icons/md';
 import './style.css';
 import DrawerComponent from '../Drawer';
+import { Link } from 'react-router-dom';
+import Thumbnail1 from '../../assets/images/3-1_thumb.jpg';
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false);
     const toggleDrawer = (newOpen) => () => {
         setOpen(newOpen);
     };
+    const SubmenuBox = ({children}) => {
+        return (
+            <div className='submenu absolute top-[170%] left-[-30%] min-w-[200px] bg-white p-5 transition-all duration-300 font-normal'>
+                {children}
+            </div>
+        );
+    };
 
     return (
         <nav>
             <div className='navbar container flex items-center justify-end p-2'>
-                <div className='col1 w-[20%] mr-2'>
+                <div className='navbar-categories col1 w-[20%] mr-2'>
                     <Button className='!text-black w-[100%]' onClick={toggleDrawer(true)}>
                         <div className='categories-btn w-[100%] flex items-center justify-between'>
                             <div className='text-section w-[60%] flex items-center justify-around'><MdSegment /> Shop By Categories</div>
@@ -24,18 +33,84 @@ const Navbar = () => {
                     </Button>
                 </div>
 
-                <div className='col2 w-[50%] !text-black font-medium border-l-2 border-gray-150'>
+                <div className='navbar-quick-links col2 w-[50%] font-medium border-l-2 border-gray-150'>
                     <ul className='flex items-center justify-around'>
-                        <li className='link list-none transition'>Home</li>
-                        <li className='link list-none transition'>Fashion</li>
-                        <li className='link list-none transition'>New Arrivals</li>
-                        <li className='link list-none transition'>All brands</li>
-                        <li className='link list-none transition'>Best Deals</li>
-                        <li className='link list-none transition'>Blog</li>
+                        <li className='list-none transition'>
+                            <Link to={'#'} className='link text-black'>Home</Link>
+                        </li>
+                        <li className='list-none transition relative'>
+                            <Link to={'#'} className='link text-black'>Fashion</Link>
+                            <SubmenuBox>
+                                <div className='flex flex-col w-[525px]'>
+                                <div className='upper flex justify-between gap-5 mb-4'>
+                                    <div className='col1 w-[175px]'>
+                                        <ul>
+                                            <li className='!text-black font-medium pb-1'>Apparel</li>
+                                            <li className='list-none pb-1'>Smart Tablet</li>
+                                            <li className='list-none pb-1'>Crepe T-Shirt</li>
+                                            <li className='list-none pb-1'>Leather Watch</li>
+                                            <li className='list-none pb-1'>Rolling Diamond</li>
+                                        </ul>
+                                    </div>
+                                    <div className='col2 w-[175px]'>
+                                    <ul>
+                                            <li className='!text-black font-medium pb-1'>Outerwear</li>
+                                            <li className='list-none pb-1'>Wooden Chair</li>
+                                            <li className='list-none pb-1'>Sneakers Shoes</li>
+                                            <li className='list-none pb-1'>Purse</li>
+                                            <li className='list-none pb-1'>Xbox Controller</li>
+                                        </ul>
+                                    </div>
+                                    <div className='col3 w-[175px]'>
+                                    <ul>
+                                            <li className='!text-black font-medium pb-1'>Footwear</li>
+                                            <li className='list-none pb-1'>Leather Watch</li>
+                                            <li className='list-none pb-1'>Cabinet Table</li>
+                                            <li className='list-none pb-1'>Headphones</li>
+                                            <li className='list-none pb-1'>Sunglasses</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='lower'>
+                                    <img src={Thumbnail1} />
+                                </div>
+                                </div>
+                            </SubmenuBox>
+                        </li>
+                        <li className='list-none transition'>
+                            <Link to={'#'} className='link text-black'>New Arrivals</Link>
+                        </li>
+                        <li className='list-none transition relative'>
+                            <Link to={'#'} className='link text-black'>All brands</Link>
+                            <SubmenuBox>
+                                <ul>
+                                    <li className='list-none pb-1'>Gadget Zone</li>
+                                    <li className='list-none pb-1'>Initech space</li>
+                                    <li className='list-none pb-1'>Looney Tunes</li>
+                                    <li className='list-none pb-1'>Massive Dynamic</li>
+                                    <li className='list-none pb-1'>Pro Tech Gear</li>
+                                    <li className='list-none pb-1'>Soylent Green</li>
+                                    <li className='list-none pb-1'>The Simpsons</li>
+                                    <li className='list-none'>Free Authority</li>
+                                </ul>
+                            </SubmenuBox>
+                        </li>
+                        <li className='list-none transition'>
+                            <Link to={'#'} className='link text-black'>Best Deals</Link>
+                        </li>
+                        <li className='list-none transition relative'>
+                            <Link to={'#'} className='link text-black'>Blog</Link>
+                            <SubmenuBox>
+                                <ul>
+                                    <li className='list-none pb-1'>Best Deals</li>
+                                    <li className='list-none'>Blog</li>
+                                </ul>
+                            </SubmenuBox>
+                        </li>
                     </ul>
                 </div>
 
-                <div className='col3 w-[30%] !text-black font-medium flex justify-end items-center'>
+                <div className='navbar-announce col3 w-[30%] !text-black font-medium flex justify-end items-center'>
                     <img src={RocketLogo} />
                     Free International Delivery
                 </div>
