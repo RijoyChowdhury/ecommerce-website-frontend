712b86b2589ec7ae83dbe2e517a7aee82a049d10
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2025-07-03 09:57:13 +0530

    feat(rating-component): adds tooltip and reactive render on passed argument

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Tooltip } from '@mui/material';
 import { FaStar } from 'react-icons/fa';
 
-const StarRating = () => {
+const StarRating = ({ value = 0, showTooltip = true }) => {
     return (
-        <>
-            <div className="star text-yellow"><FaStar /></div>
-            <div className="star text-yellow"><FaStar /></div>
-            <div className="star text-yellow"><FaStar /></div>
-            <div className="star text-custom-gray"><FaStar /></div>
-            <div className="star text-custom-gray"><FaStar /></div>
-        </>
+        <Tooltip title={`Rating: ${value}`} placement="top" arrow>
+            <div className='flex'>
+                {new Array(Math.floor(value)).fill(0).map((val, index) => <div key={index} className="star text-yellow"><FaStar /></div>)}
+                {new Array(Math.ceil(5 - value)).fill(0).map((val, index) => <div className="star text-custom-gray"><FaStar /></div>)}
+            </div>
+        </Tooltip>
     )
 };
 
