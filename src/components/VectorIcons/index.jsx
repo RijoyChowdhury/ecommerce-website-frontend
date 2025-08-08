92beab0fee946065948d6e5f559a860e9e55b72d
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2025-08-08 12:48:07 +0530

    feat(vector-images): adds vector images

---
+++ src/components/VectorIcons/index.jsx
@@ -0,0 +1,17 @@
+import masterCard from "./masterCard";
+import visaCard from "./visaCard";
+import amexCard from "./amexCard";
+import payPal from "./payPal";
+import applePay from "./applePay";
+import googlePay from "./googlePay";
+import affirm from "./affirm";
+
+export {
+    masterCard as MasterCard,
+    visaCard as VisaCard,
+    amexCard as AmexCard,
+    payPal as PayPal,
+    applePay as ApplePay,
+    googlePay as GooglePay,
+    affirm as AffirmPay,
+}
